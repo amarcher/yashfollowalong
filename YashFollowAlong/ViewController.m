d0714cd6aff0f92e495bc958a73622df5585e559
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-20 20:47:32 -0800

    working prototype

--- YashFollowAlong/ViewController.m
+++ YashFollowAlong/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *fifteenPercentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eighteenPercentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *twentyPercentLabel;
 
 @end
 
@@ -17,6 +21,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSString *)calculateTipAmountFor:(float)tipAmount {
+    
+    float tip = tipAmount/100;
+    float totalAmount = [self.amountTextField.text floatValue];
+    float calculatedTipAmount = tip * totalAmount;
+    NSString * totalString = [NSString stringWithFormat:@"$%.02f", calculatedTipAmount];
+    return totalString;
+}
+
+- (IBAction)calculateTip:(id)sender {
+    
+    self.fifteenPercentLabel.text = [self calculateTipAmountFor:15];
+    self.eighteenPercentLabel.text = [self calculateTipAmountFor:18];
+    self.twentyPercentLabel.text = [self calculateTipAmountFor:20];
+
 }
 
 - (void)didReceiveMemoryWarning {
